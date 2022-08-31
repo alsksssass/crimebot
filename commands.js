3453162bfb0cd29b9d1014e46caa2813268e8db5
alsksssass <112150208+alsksssass@users.noreply.github.com>
2022-08-31 18:02:52 +0900

    Update commands.js

--- commands.js
+++ commands.js
@@ -5,12 +5,12 @@ const { clientId } = require('./config.json');
 
 
 const commands = [
-  new SlashCommandBuilder().setName('청소').setDescription('청소갯수').addNumberOption(option => option.setName('숫자1').setDescription('최대 99개')).setDefaultMemberPermissions(0),
+  new SlashCommandBuilder().setName('청소').setDescription('청소갯수').addNumberOption(option => option.setName('숫자').setDescription('최대 99개')).setDefaultMemberPermissions(0),
   new SlashCommandBuilder().setName('청소기').setDescription('메시지300개 삭제').setDefaultMemberPermissions(0),
   new SlashCommandBuilder().setName('말').setDescription('진행자말 일반').addStringOption(option => option.setName('그냥말').setDescription('메시지넣기')).setDefaultMemberPermissions(0),
   new SlashCommandBuilder().setName('진행').setDescription('진행자말 박스').addStringOption(option => option.setName('박스말').setDescription('메시지넣기')).setDefaultMemberPermissions(0),
   new SlashCommandBuilder().setName('딜레이').setDescription('지연 전송할 말').addStringOption(option => option.setName('딜레이말').setDescription('메시지넣기')).addNumberOption(option => option.setName('시간').setDescription('ex)1분=60,max15분')).setDefaultMemberPermissions(0),
-  new SlashCommandBuilder().setName('딜레이박스').setDescription('지연 전송할 말').addStringOption(option => option.setName('딜레이말').setDescription('메시지넣기')).addNumberOption(option => option.setName('시간').setDescription('ex)1분=60,max15분')).setDefaultMemberPermissions(0),
+  new SlashCommandBuilder().setName('딜레이박스').setDescription('지연 전송할 말').addStringOption(option => option.setName('딜레이말1').setDescription('메시지넣기')).addNumberOption(option => option.setName('시간').setDescription('ex)1분=60,max15분')).setDefaultMemberPermissions(0),
 
 ]
   .map(command => command.toJSON());
