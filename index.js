91dff313a42a8145a90c424a063c3c8791738114
alsksssass <112150208+alsksssass@users.noreply.github.com>
2022-08-31 20:10:44 +0900

    Update index.js

--- index.js
+++ index.js
@@ -153,19 +153,20 @@ client.on('interactionCreate', async interaction => {
   }
    else if (commandName === '타이머') {
     const number = interaction.options.getNumber('타임');
+    const wait = require('node:timers/promises').setTimeout;///딜레이 구문
     let time = number * 1000
     let time1 = time -= 1000
     let qks = time1 /= 2
     let ch = qks /= 1000
     const tlwkr = '초 카운트 시작'
     const skadma55 = '초 남음'
-    const message1 = await interaction.reply(number + tlwkr)
+    const message1 = await interaction.reply({ content: ekdha + number + tlwkr + ekdha, fetchReply: true })
     message1.react('❤️')
-    await wait(qks);
+    await wait(time1);
     const message2 = await interaction.channel.send(ekdha + ch + skadma55 + ekdha)
     message2.react('❤️')
-    await wait(qks);
-    const message3 = await interaction.channel.send('타이머 종료되었습니다!')
+    await wait(time1);
+    const message3 = await interaction.channel.send('```타이머 종료되었습니다!```')
     message3.react('❤️').then(console.log).catch(console.error);
   }
 
