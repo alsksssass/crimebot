a6d37ef7223b4e1f6d621a961752a7594bf952c7
alsksssass <112150208+alsksssass@users.noreply.github.com>
2022-08-31 19:06:12 +0900

    Update commands.js

--- commands.js
+++ commands.js
@@ -11,6 +11,15 @@ const commands = [
   new SlashCommandBuilder().setName('진행').setDescription('진행자말 박스').addStringOption(option => option.setName('박스말').setDescription('메시지넣기')).setDefaultMemberPermissions(0),
   new SlashCommandBuilder().setName('딜레이').setDescription('지연 전송할 말').addStringOption(option => option.setName('딜레이말').setDescription('메시지넣기')).addNumberOption(option => option.setName('시간').setDescription('ex)1분=60,max15분')).setDefaultMemberPermissions(0),
   new SlashCommandBuilder().setName('딜레이박스').setDescription('지연 전송할 말').addStringOption(option => option.setName('딜레이말1').setDescription('메시지넣기')).addNumberOption(option => option.setName('시간1').setDescription('ex)1분=60,max15분')).setDefaultMemberPermissions(0),
+  new SlashCommandBuilder().setName('1').setDescription('용의자 카운트!').setDefaultMemberPermissions(0),
+  new SlashCommandBuilder().setName('2').setDescription('용의자 카운트').setDefaultMemberPermissions(0),
+  new SlashCommandBuilder().setName('3').setDescription('용의자 카운트').setDefaultMemberPermissions(0),
+  new SlashCommandBuilder().setName('4').setDescription('용의자 카운트').setDefaultMemberPermissions(0),
+  new SlashCommandBuilder().setName('5').setDescription('용의자 카운트').setDefaultMemberPermissions(0),
+  new SlashCommandBuilder().setName('6').setDescription('용의자 카운트').setDefaultMemberPermissions(0),
+  new SlashCommandBuilder().setName('0').setDescription('용의자 카운트 리셋').setDefaultMemberPermissions(0),
+  new SlashCommandBuilder().setName('뷰').setDescription('카운트 현황').setDefaultMemberPermissions(0),
+]
 
 ]
   .map(command => command.toJSON());
